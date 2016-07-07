2b817c8975e59e73a55a8e96e1396863be7b09ff
Simon Posada Fishman <spf@fb.com>
2016-07-07 13:19:12 -0700

    Add model user class that extracts profile info from fb

---
+++ Main/JamChat/JamChat/User.swift
@@ -0,0 +1,124 @@
+//
+//  User.swift
+//  JamChat
+//
+//  Created by Simon Posada Fishman on 7/7/16.
+//  Copyright © 2016 Jammers. All rights reserved.
+//
+
+import UIKit
+import ParseUI
+import ParseFacebookUtilsV4
+import FBSDKCoreKit
+
+class User: NSObject {
+    
+    static private(set) var currentUser: User? = nil
+    
+    static let loginDelegate = LoginDelegate()
+    
+    var facebookID: String!
+    var name: String!
+    var pictureURL: NSURL!
+    var email: String!
+    
+    /**
+     Initializes user based on a facebookID, pulling all the user data from the Facebook API.
+     */
+    init(userId: String) {
+        super.init()
+        // Create request for user's Facebook data
+        let request = FBSDKGraphRequest(graphPath: userId, parameters:["fields" : "email,name,friends"])
+        
+        // Send request to Facebook
+        request.startWithCompletionHandler {
+            
+            (connection, result, error) in
+            
+            if error != nil {
+                // Some error checking here
+            }
+            else if let userData = result as? [String:AnyObject] {
+                
+                print(userData)
+                self.facebookID = userData["id"] as! String
+                self.name = userData["name"] as! String
+                self.email = userData["email"] as? String
+                self.pictureURL = NSURL(string: "https://graph.facebook.com/\(self.facebookID)/picture?type=large&return_ssl_resources=1")!
+            }
+        }
+    }
+    
+    /**
+     Checks if an user is already logged in
+     */
+    class func isLoggedIn() -> Bool {
+        if PFUser.currentUser() != nil && currentUser != nil {
+            return PFFacebookUtils.isLinkedWithUser(PFUser.currentUser()!)
+        }
+        
+        return false
+    }
+    
+    /**
+     Logs out the current user, it is recommended to call login() immediately after.
+     */
+    class func logout() {
+        PFFacebookUtils.unlinkUserInBackground(PFUser.currentUser()!)
+        PFUser.logOut()
+        currentUser = nil
+        FBSDKLoginManager().logOut()
+    }
+    
+    /**
+     Attempts to log in an user, presenting a login subview in the given view.
+     
+     - parameters:
+        - controller: view controller on which to present the login subview
+        - success: block that will be called when the user is logged in succesfully
+        - failure: block that will be called when there's an error
+    */
+    class func login(controller: UIViewController, success: (() -> ())?, failure: ((NSError?) -> ())?) {
+        loginDelegate.loginSuccess = success
+        loginDelegate.loginFailure = failure
+        loginDelegate.controller = controller
+        
+        // Initialize login view
+        let loginViewController = PFLogInViewController()
+        loginViewController.delegate = loginDelegate
+        
+        // Configure custom login
+        loginViewController.fields = [.UsernameAndPassword, .LogInButton, .PasswordForgotten, .Facebook]
+        loginViewController.facebookPermissions = ["email", "public_profile", "user_friends"]
+        loginViewController.emailAsUsername = true
+        
+        // Present view
+        controller.presentViewController(loginViewController, animated: false, completion: nil)
+    }
+    
+    
+}
+
+// Delegate used for the login view success and failure cases
+class LoginDelegate: NSObject, PFLogInViewControllerDelegate {
+    
+    var loginSuccess: (() -> ())? = nil
+    var loginFailure: ((NSError?) -> ())? = nil
+    var controller: UIViewController?
+    
+    // Called when user is logged in succesfully, dismissing the login view
+    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
+        User.currentUser = User(userId: "me")
+        controller!.dismissViewControllerAnimated(true, completion: nil)
+        if let success = loginSuccess {
+            success()
+        }
+    }
+    
+    // Called when the login fails
+    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
+        if let failure = loginFailure {
+            failure(error)
+        }
+    }
+}
--- Main/JamChat/JamChat/ViewController.swift
+++ Main/JamChat/JamChat/ViewController.swift
@@ -12,7 +12,7 @@ import FBSDKLoginKit
 import Parse
 import ParseUI
 
-class ViewController: UIViewController, PFLogInViewControllerDelegate {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,18 +21,9 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        print(PFUser.currentUser()?.username)
-        if (PFUser.currentUser() == nil) {
-            let loginViewController = PFLogInViewController()
-            
-            loginViewController.delegate = self
-            
-            loginViewController.fields = [.UsernameAndPassword, .LogInButton, .PasswordForgotten, .SignUpButton, .Facebook]
-            loginViewController.emailAsUsername = true
-            
-            self.presentViewController(loginViewController, animated: false, completion: nil)
-        } else {
-            self.presentLoggedInAlert()
+
+        if (!User.isLoggedIn()) {
+            User.login(self, success: nil, failure: nil)
         }
     }
 
@@ -41,26 +32,5 @@ class ViewController: UIViewController, PFLogInViewControllerDelegate {
         // Dispose of any resources that can be recreated.
     }
     
-    func presentLoggedInAlert() {
-        let alertController = UIAlertController(title: "You're logged in", message: "Welcome to ParseChat", preferredStyle: .Alert)
-        let OKAction = UIAlertAction(title: "OK", style: .Default) { (action) in
-            self.dismissViewControllerAnimated(true, completion: nil)
-        }
-        alertController.addAction(OKAction)
-        self.presentViewController(alertController, animated: true, completion: nil)
-    }
-    
-    func logInViewController(logInController: PFLogInViewController, didLogInUser user: PFUser) {
-        
-        self.dismissViewControllerAnimated(true, completion: nil)
-        presentLoggedInAlert()
-    }
-    
-    func logInViewController(logInController: PFLogInViewController, didFailToLogInWithError error: NSError?) {
-        
-        print(error?.localizedDescription)
-    }
-
-
 }
 
