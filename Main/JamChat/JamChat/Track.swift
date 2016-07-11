d81f78a41959320815f5d893144a9be9909efaf5
Simon Posada Fishman <spf@fb.com>
2016-07-11 10:51:50 -0700

    Created model files

---
+++ Main/JamChat/JamChat/Track.swift
@@ -0,0 +1,13 @@
+//
+//  Track.swift
+//  JamChat
+//
+//  Created by Simon Posada Fishman on 7/9/16.
+//  Copyright © 2016 Jammers. All rights reserved.
+//
+
+import UIKit
+
+class Track: NSObject {
+
+}
