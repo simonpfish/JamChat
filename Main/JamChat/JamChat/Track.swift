5354f1e5822fe3dbd46c97f6669253d85449afc6
Simon Posada Fishman <spf@fb.com>
2016-07-11 11:05:08 -0700

    Improved models and updated submodules

--- Main/JamChat/JamChat/Track.swift
+++ Main/JamChat/JamChat/Track.swift
@@ -7,7 +7,36 @@
 //
 
 import UIKit
+import Parse
+import AudioKit
 
 class Track: NSObject {
-
+    
+    
+    private(set) var author: User!
+    
+    private var player: AKAudioPlayer?
+    private var exportedPath: String?
+    private var file: PFFile!
+    
+    init(object: PFObject) {
+        file = object["media"] as! PFFile
+        let parseUser = object["author"] as! PFUser
+        author = User(user: parseUser)
+    }
+    
+    
+    /**
+     Uploads track to parse
+     */
+    func upload(completion: PFBooleanResultBlock?) {
+        // Create Parse object
+        let object = PFObject(className: "Track")
+        
+        // Add relevant fields to the object
+        object["media"] = PFFile(name: "audio.m4a", data: NSData(contentsOfFile: exportedPath!)!)
+        object["author"] = author.parseUser // Pointer column type that points to PFUser
+        
+        object.saveInBackgroundWithBlock(completion)
+    }
 }
