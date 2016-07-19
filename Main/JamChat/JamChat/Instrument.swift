c368dcae95092dbf4238736b64ccfa0884806ccb
Simon Posada Fishman <spf@fb.com>
2016-07-19 11:02:00 -0700

    Improved instrument model with static elements

--- Main/JamChat/JamChat/Instrument.swift
+++ Main/JamChat/JamChat/Instrument.swift
@@ -11,45 +11,37 @@ import AudioKit
 
 class Instrument: NSObject {
     
-        private(set) var _name: String?
-        private(set) var _color: UIColor?
-        private(set) var _sound: String?
+    static let mainMixer = AKMixer()
     
-    func setInstrument(name: String) {
-        _name = name
+    private(set) var name: String!
+    private(set) var color: UIColor!
+    private(set) var image: UIImage?
     
+    private let sampler =  AKSampler()
+    
+    private init(name: String, color: UIColor, image: UIImage?, fileName: String) {
+        self.name = name
+        self.color = color
+        self.image = image
+        sampler.loadWav(fileName)
         
-        if(_name == "ElectricGuitar"){
-            _color = UIColor(red: 0, green: 0.7882, blue: 0.7608, alpha: 1.0)
-            _sound = "RockGuitarC"
-        }
-            
-        else if(_name == "Saxophone"){
-            _color = UIColor(red: 0.9294, green: 0.8353, blue: 0, alpha: 1.0)
-            _sound = "SaxC"
-        }
-            
-        else if(_name == "Choir"){
-            _color = UIColor(red: 0.6392, green: 0, blue: 0.6275, alpha: 1.0)
-            _sound = "ChoirC"
-        }
-            
-        else if(_name == "Piano"){
-            _color = UIColor.whiteColor()
-            _sound = "PianoC"
-        }
-            
-        else if(_name == "AcousticBass"){
-            _color = UIColor(red: 0.5294, green: 0.1294, blue: 0, alpha: 1.0)
-            _sound = "AcousticBassC"
-        }
-            
-        else if(_name == "ElectricBass"){
-            _color = UIColor(red: 0.4118, green: 0.6863, blue: 0, alpha: 1.0)
-            _sound = "ElectricBassC"
-        }
-
+        Instrument.mainMixer.connect(self.sampler)
     }
     
-    
+    func play(note: Int) {
+        sampler.playNote(note)
     }
+    
+    func stop() {
+        sampler.stopNote()
+    }
+    
+    // Instruments:
+    static let electricGuitar = Instrument(name: "Electric Guitar", color: UIColor(red: 0, green: 0.7882, blue: 0.7608, alpha: 1.0), image: nil, fileName: "RockGuitarC")
+    static let saxophone = Instrument(name: "Electric Guitar", color: UIColor(red: 0.9294, green: 0.8353, blue: 0, alpha: 1.0), image: nil, fileName: "SaxC")
+    static let choir = Instrument(name: "Electric Guitar", color: UIColor(red: 0.6392, green: 0, blue: 0.6275, alpha: 1.0), image: nil, fileName: "ChoirC")
+    static let piano = Instrument(name: "Electric Guitar", color: UIColor.whiteColor(), image: nil, fileName: "PianoC")
+    static let acousticBass = Instrument(name: "Electric Guitar", color: UIColor(red: 0.5294, green: 0.1294, blue: 0, alpha: 1.0), image: nil, fileName: "AcousticBassC")
+    static let electricBass = Instrument(name: "Electric Guitar", color: UIColor(red: 0.4118, green: 0.6863, blue: 0, alpha: 1.0), image: nil, fileName: "ElectricBassC")
+    
+}
