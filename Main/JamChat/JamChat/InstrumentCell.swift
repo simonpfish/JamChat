6b981c8adf379626256b4af6dd312f258664597c
Meena Sengottuvelu <meena97@fb.com>
2016-07-21 13:19:46 -0700

    created user and instrument collection view delegates

---
+++ Main/JamChat/JamChat/InstrumentCell.swift
@@ -0,0 +1,15 @@
+//
+//  InstrumentCell.swift
+//  JamChat
+//
+//  Created by Meena Sengottuvelu on 7/21/16.
+//  Copyright © 2016 Jammers. All rights reserved.
+//
+
+import UIKit
+
+class InstrumentCell: UICollectionViewCell {
+    
+    var instrument: Instrument!
+    
+}
