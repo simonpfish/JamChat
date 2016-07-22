ca831e48da764bca57897cdacebbe3475c7904c0
Meena Sengottuvelu <meena97@fb.com>
2016-07-21 22:10:30 -0700

    Added table view to jam creation view controller to allow user to scroll through their friends.

---
+++ Main/JamChat/JamChat/FriendCell.swift
@@ -0,0 +1,40 @@
+//
+//  FriendCell.swift
+//  JamChat
+//
+//  Created by Meena Sengottuvelu on 7/21/16.
+//  Copyright © 2016 Jammers. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+
+    @IBOutlet weak var profilePictureView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    var user: User! {
+        didSet {
+            if let nameLabel = nameLabel {
+                nameLabel.text = user.name
+            }
+            profilePictureView.setImageWithURL(user.profileImageURL)
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        // Make image circular:
+        profilePictureView.layer.cornerRadius = profilePictureView.frame.size.width / 2;
+        profilePictureView.clipsToBounds = true;
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+
+}
