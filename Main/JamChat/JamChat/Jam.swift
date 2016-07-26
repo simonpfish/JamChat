942cd91d8a38aa6b89a5410f1fe84fbecc2281e6
Meena Sengottuvelu <meena97@fb.com>
2016-07-26 16:31:08 -0700

    added comments and print statements

--- Main/JamChat/JamChat/Jam.swift
+++ Main/JamChat/JamChat/Jam.swift
@@ -284,7 +284,6 @@ class Jam: NSObject {
                 for object in objects ?? [] {
                     jams.append(Jam(object: object))
                     jams.last?.loadData({
-                        print("Loading jam \(Jam(object: object))")
                         loadedCount += 1
                         if loadedCount == objects!.count {
                             print("Succesfully downloaded jams for user: \(specificUser.name!)")
