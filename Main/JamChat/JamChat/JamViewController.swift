9d7b1143fd27d22ea1f22b71f30033be31999dc8
Meena Sengottuvelu <meena97@fb.com>
2016-07-29 16:24:39 -0700

    hide record button on loop keyboard

--- Main/JamChat/JamChat/JamViewController.swift
+++ Main/JamChat/JamChat/JamViewController.swift
@@ -352,6 +352,7 @@ class JamViewController: UIViewController, UICollectionViewDelegate, UICollectio
     loopButton.setImage(UIImage(named:"back_arrow.png"), forState: .Normal)
             inKeyboard = false
             keyboardButton.hidden = true
+            recordView.hidden = true
         }
             
         else{
@@ -360,6 +361,7 @@ class JamViewController: UIViewController, UICollectionViewDelegate, UICollectio
             loopButton.setImage(UIImage(named:"loop.png"), forState: .Normal)
             inKeyboard = true
             keyboardButton.hidden = false
+            recordView.hidden = false
         }
     }
 
